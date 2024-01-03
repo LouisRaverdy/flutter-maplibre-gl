4f8d730e09974021e74830dd1fd9f8cddcea538d
Louis <47295080+LouisRaverdy@users.noreply.github.com>
2024-01-03 22:30:01 +0100

    Delete maplibre_gl_web directory

--- maplibre_gl_web/lib/src/style/layers/layer.dart
+++
@@ -1,9 +0,0 @@
-library maplibre.style.layers.layer;
-
-abstract class Layer {
-  /// JS object.
-  dynamic get jsObject => throw Exception('jsObject not implemented!');
-
-  /// Dict object.
-  Map<String, dynamic> get dict => throw Exception('dict not implemented!');
-}
